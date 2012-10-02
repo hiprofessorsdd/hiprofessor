7c4b16421d00aef6ed9b467b3c5f45099c437e1b
hiprofessor <hiprofessorssd@gmail.com>
2012-10-02 00:29:00 +0000

    -pull_schedule.sh now calls the new python script if wget was successful.
      -TODO.txt to keep track of things to do for the frontend.

--- pull_schedules/pull_schedule.sh
+++ pull_schedules/pull_schedule.sh
@@ -17,3 +17,10 @@ fi
 SCHED_PULL_URL_BASE="http://sis.rpi.edu/reg/zs"
 SCHED_PULL_URL_SUFF=".htm"
 wget -q -O $2$1.htm $SCHED_PULL_URL_BASE$2$SCHED_PULL_SEMESTER$SCHED_PULL_URL_SUFF
+SCHED_PULL_WGET_RET=$?
+if [ $SCHED_PULL_WGET_RET -ne "0" ]; then
+  echo -e "wget returns $SCHED_PULL_WGET_RET"
+  echo -e "\e[00;31mERROR: Schedule download failed. Check that the semester and year are valid. The schedules might not be published yet. If all else fails, email hiprofessorsdd@gmail.com to report an error.\e[00m"
+else
+  python parse_sched.py $2$1.htm
+fi
