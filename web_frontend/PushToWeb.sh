fd8188e89448b4a1db36ca0011ac5c6dfbb0c22f
hiprofessor <hiprofessorssd@gmail.com>
2012-11-03 02:14:01 +0000

    PushToWeb.sh is now much safer. Before doing any copying, it scans the directories for *.swp files, indicating that a user might be actively editing and that the brute copy that it does might not be a good idea. The user can override these warnings, but it gets noted in the log file. -Tim Treese

--- web_frontend/PushToWeb.sh
+++ web_frontend/PushToWeb.sh
@@ -7,10 +7,56 @@ then
   echo -e "Push to web cancelled"
   exit
 fi
+
+#check for swap files in either directory and further warn user:
+_SwapForce=
+declare -a _Directories=(/var/www /usr/lib/cgi-bin)
+for directory in $_Directories
+do
+  cd $directory
+  for swap in *.swp
+  do
+    if [ "$swap" != "*.swp" ]
+    then
+      echo -e "\e[033mWARNING: $directory/$swap is present! Another user is likely editing text in there, and their work will be overwritten. Continue? [Y/n]"
+      read -e _Continue
+      echo -e "\e[037m"
+      if [ $_Continue != "Y" ]
+      then
+        echo -e "Push to web cancelled!"
+        exit
+      else
+        _SwapForce="*with swap forcing*"
+      fi
+    fi
+  done
+  for swap in .?*.swp
+  do
+    if [ "$swap" != "*.swp" ]
+    then
+      echo -e "\e[033mWARNING: $directory/$swap is present! Another user is likely editing text in there, and their work will be overwritten. Continue? [Y/n]"
+      read -e _Continue
+      echo -e "\e[037m"
+      if [ $_Continue != "Y" ]
+      then
+        echo -e "Push to web cancelled!"
+        exit
+      else
+        _SwapForce="*with swap forcing*"
+      fi
+    fi
+  done
+done
+
+cd /home/ubuntu/hiprofessor_git/hiprofessor/web_frontend
+
 echo -e "Enter your RCSID"
 read -e _PusherRCSID
-echo -e "\e[031m`date`\e[037m by $_PusherRCSID" >> PushLogs/webPush.log
-echo -e "\e[031m`date`\e[037m by $_PusherRCSID" >> PushLogs/wwwPush.log
-sudo cp -r www /var/www
-echo -e "\e[031m`date`\e[037m by $_PusherRCSID" >> PushLogs/cgiPush.log
-sudo cp -r cgi-bin /usr/lib/cgi-bin
+if [ -d PushLogs ];
+then
+  echo -e "\e[031m`date`\e[037m by $_PusherRCSID $_SwapForce" >> PushLogs/webPush.log
+  echo -e "\e[031m`date`\e[037m by $_PusherRCSID $_SwapForce" >> PushLogs/wwwPush.log
+  sudo cp -r www /var
+  echo -e "\e[031m`date`\e[037m by $_PusherRCSID $_SwapForce" >> PushLogs/cgiPush.log
+  sudo cp -r cgi-bin /usr/lib
+fi
