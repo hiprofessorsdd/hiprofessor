cfa1d23a9b52a5dfb0927076ec8d528aa6630af5
hiprofessor <hiprofessorssd@gmail.com>
2012-11-04 15:29:46 -0500

    Updating map.js with the loadClass() function.

--- web_frontend/www/map.js
+++ web_frontend/www/map.js
@@ -25,12 +25,9 @@ function dropTag(){
 	var mark = new google.maps.Marker({
 		position: point,
 		animation: google.maps.Animation.DROP,
-		title:"Gay"
+		title:"Brian Rules"
 	});
 	mark.setMap(map);
-	mark.setMap(map);
-	courseInfoWindow = new google.maps.InfoWindow({content: 'You are Gay and you should feel Gay!'});
-	google.maps.event.addListener(mark, 'click', function() {courseInfoWindow.open(map,mark);});
 	return false;
 }
 
@@ -47,9 +44,32 @@ function randTags(){
 		var mark = new google.maps.Marker({
 		position: point,
 		animation: google.maps.Animation.DROP,
-		title:"Gay"
+		title:"Brian Rules"
 		});
 		mark.setMap(map);
 	}
+	return false;
+}
+function loadClass(Lat,Lng,build,room,dept,c_no,title,start,end,days,type){
+	if(isNaN(Lat) || isNaN(Lng)){
+		alert("Latitude: [-90, 90], Longitude: [-180,180].");
+		return false;
+	}
+	if(Lat < -90 || Lat > 90 || Lng < -180 || Lng > 180){
+		alert("Latitude: [-90, 90], Longitude: [-180,180].");
+		return false;
+	}
+	var point = new google.maps.LatLng(Lat, Lng);
+	var mark = new google.maps.Marker({
+		position: point,
+		animation: google.maps.Animation.DROP,
+		title: dept+' '+c_no,
+		info: '<html><body><div id="some_text" style="Float:left; width:auto; height: auto; margin: 5px; padding: 5px; border: 1px solid black; color:#C40000; font-family:verdana; font-size:15p    x; "><b>'+dept+" "+c_no+' - '+title+'</b><p style="font-size:12px">'+build+' '+room+'<br/>'+days+'<br/>'+start+' - '+end+'<br/>'+type+'</p></div></body></html>'
+	});
+	mark.setMap(map);
+	courseInfoWindow = new google.maps.InfoWindow();
+	google.maps.event.addListener(mark, 'click', function() {
+	courseInfoWindow.setContent(this.info);
+	courseInfoWindow.open(map,this);});
 	return false;
 }
